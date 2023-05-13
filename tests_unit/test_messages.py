8b177bce4fd986370920c064a27a1c9654b2f356
Emanuel Trinc <emanuel.trinc@yahoo.com>
2023-05-14 00:23:27 +0300

    Fix integration tests

--- tests_unit/test_messages.py
+++ tests_unit/test_messages.py
@@ -4,7 +4,7 @@ import services.rest_api_service as restApiService
 from requests import Response
 import time
 
-def test_send_message():
+def test_case1():
     """
     """
     nodeInstance = node.Node()
@@ -18,3 +18,20 @@ def test_send_message():
     response: Response = restService.post_request(url, body)
     assert response.status_code == 422
     assert response.json()['error'] == 'Failed to find automatic path'
+
+def test_case2():
+    """
+    """
+    nodeInstance = node.Node()
+    recipient = nodeInstance.get_peer_id(2)
+    path = nodeInstance.get_peer_id(3)
+    body = {
+        "body": "Hello from future",
+        "recipient": recipient,
+        path: [path]
+    }
+    url = 'http://localhost:13301/api/v2/{}'.format(urls.Urls.MESSAGES_SEND)
+    restService = restApiService.RestApiService(nodeInstance.get_auth_token())
+    response: Response = restService.post_request(url, body)
+    assert response.status_code == 202
+    # assert response.json()['error'] == 'Failed to find automatic path'
