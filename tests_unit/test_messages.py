a3c61497f5a7d83515d1fe1a699653f71e4d8379
Emanuel Trinc <emanuel.trinc@yahoo.com>
2023-05-13 23:07:02 +0300

    Fix integration tests

--- tests_unit/test_messages.py
+++ tests_unit/test_messages.py
@@ -2,6 +2,7 @@ import api_object_model.node as node
 import test_data.urls as urls
 import services.rest_api_service as restApiService
 from requests import Response
+import time
 
 def test_send_message():
     """
@@ -12,7 +13,19 @@ def test_send_message():
         "body": "Hello from future",
         "recipient": recipient
     }
-    url = 'http://localhost:13301/api/v2/messages/'
+    url = 'http://localhost:13301/api/v2/{}'.format(urls.Urls.MESSAGES_SEND)
     restService = restApiService.RestApiService(nodeInstance.get_auth_token())
+    count = 0
+    while True:
+        if count == 40:
+            break
+        try:
+            response: Response = restService.post_request(url, body)
+            print("Response: {}".format(response.json()))
+            if response.status_code == 202:
+                break
+        except:
+            time.sleep(10)
+        count = count + 1
     response: Response = restService.post_request(url, body)
     assert response.status_code == 202
