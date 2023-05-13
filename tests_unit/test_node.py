5f213d3273956f3bb3d1ec6aa952f3755a0322d2
Emanuel Trinc <emanuel.trinc@yahoo.com>
2023-05-13 12:16:30 +0300

    Add node unit test

--- tests_unit/test_node.py
+++ tests_unit/test_node.py
@@ -3,6 +3,19 @@ import logging
 
 logger = logging.getLogger('test_node')
 
+def test_peer_id():
+    nodeInstance = node.Node()
+    peerId1 = nodeInstance.get_peer_id(1)
+    peerId2 = nodeInstance.get_peer_id(2)
+    peerId3 = nodeInstance.get_peer_id(3)
+    peerId4 = nodeInstance.get_peer_id(4)
+    peerId5 = nodeInstance.get_peer_id(5)
+    assert peerId1.startswith("16U")
+    assert peerId2.startswith("16U")
+    assert peerId3.startswith("16U")
+    assert peerId4.startswith("16U")
+    assert peerId5.startswith("16U")
+
 def test_last_seen():
     nodeInstance = node.Node()
     for nodeIndex in range(5, 6):
